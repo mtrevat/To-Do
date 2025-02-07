b580f1283b8f597c4d67543ab68951bd3cb42816
Mark Trevathan <marktrevathan@Marks-MacBook-Air.local>
2025-02-06 20:56:32 -0500

    initial commit

---
+++ app/models.py
@@ -0,0 +1,8 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from wtforms import TextAreaField
+from wtforms.validators import Length
+from app import db
+
+class Task:
+    pass
---
+++ todo.py
@@ -0,0 +1,10 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db}
+
+if __name__ == '__main__':
+    app.run(debug=True)
